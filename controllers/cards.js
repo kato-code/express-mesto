e031a6f3ac1052bd334617ae293cbe7f5b916b35
Katya Ksenofontova <dkurach@MacBook-Air-Dmitry.local>
2021-03-29 12:54:16 +0300

    fixed

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,203 +1,99 @@
-// /* eslint-disable eqeqeq */
-// const Card = require('../models/card.js');
+/* eslint-disable eqeqeq */
+const Card = require('../models/card.js');
 
-// const BadRequestError = require('../errors/BadRequestError.js');
-// const NotFoundError = require('../errors/NotFoundError.js');
-// const AuthorizationError = require('../errors/AuthorizationError.js');
+const BadRequestError = require('../errors/BadRequestError.js');
+const NotFoundError = require('../errors/NotFoundError.js');
+const AuthorizationError = require('../errors/AuthorizationError.js');
 
-// // получить список карточек
-// const getCards = (req, res, next) => {
-//   Card.find({})
-//     .then((cards) => res.status(200).send(cards))
-//     .catch(next);
-// };
-
-// // создать карточку
-// const createCard = (req, res, next) => {
-//   const { name, link } = req.body;
-//   const owner = req.user._id;
-
-//   Card.create({ name, link, owner })
-//     .then((card) => res.status(200).send(card))
-//     .catch((error) => {
-//       if (error.name === 'ValidationError') {
-//         throw new BadRequestError('Введены некорректные данные');
-//       }
-//       next(error);
-//     });
-// };
-
-// // удалить карточку
-// const deleteCard = (req, res, next) => {
-//   Card.findById(req.params.cardId)
-//     .then((card) => {
-//       if (!card) {
-//         throw new NotFoundError('Запрашиваемые данные не найдены');
-//       }
-//       if (card.owner.toString() != req.user._id) {
-//         throw new AuthorizationError('Недостаточно прав');
-//       }
-//       // return Card.findByIdAndDelete(req.params._id);
-//       Card.deleteOne(card)
-//         .then((data) => {
-//           res.send({ messages: `Карточка с id: ${data._id} успешно удалена` });
-//         })
-//         .catch(next);
-//     });
-//   // .then(() => {
-//   //   res.status(200).send({ messages: 'Карточка удалена' });
-//   // })
-//   // .catch((error) => {
-//   //   if (error.name === 'CastError') {
-//   //     throw new BadRequestError('Карточка с таким id не найдена');
-//   //   }
-//   //   next(error);
-//   // });
-// };
-
-// // поставить лайк
-// const likeCard = (req, res, next) => {
-//   Card.findByIdAndUpdate(
-//     req.params.cardId,
-//     { $addToSet: { likes: req.user._id } },
-//     { new: true },
-//   )
-//     .then((card) => {
-//       if (!card) {
-//         throw new NotFoundError('Карточка с таким id не найдена');
-//       }
-//       res.status(200).send(card);
-//     })
-//     .catch((error) => {
-//       if (error.name === 'CastError') {
-//         throw new BadRequestError('Карточка с таким id не найдена');
-//       }
-//       next(error);
-//     });
-// };
-
-// // убрать лайк
-// const dislikeCard = (req, res, next) => {
-//   Card.findByIdAndUpdate(
-//     req.params.cardId,
-//     { $pull: { likes: req.user._id } },
-//     { new: true },
-//   )
-//     .then((card) => {
-//       if (!card) {
-//         throw new NotFoundError('Карточка с таким id не найдена');
-//       }
-//       res.status(200).send(card);
-//     })
-//     .catch((error) => {
-//       if (error.name === 'CastError') {
-//         throw new BadRequestError('Карточка с таким id не найдена');
-//       }
-//       next(error);
-//     });
-// };
-
-// module.exports = {
-//   getCards,
-//   createCard,
-//   deleteCard,
-//   likeCard,
-//   dislikeCard,
-// };
-
-const Card = require('../models/card');
-
-const NotFoundError = require('../errors/NotFoundError');
-const BadRequestError = require('../errors/BadRequestError');
-const AuthorizationError = require('../errors/AuthorizationError');
-
-// Запрос списка карточек
+// получить список карточек
 const getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.status(200).send(cards))
     .catch(next);
 };
 
-// Запрос на создание карточки
+// создать карточку
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
+  const owner = req.user._id;
 
-  Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные');
-      } else {
-        next(err);
+  Card.create({ name, link, owner })
+    .then((card) => res.status(200).send(card))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        throw new BadRequestError('Введены некорректные данные');
       }
+      next(error);
     });
 };
 
-// Запрос на удаление карточки по идентификатору
+// удалить карточку
 const deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
-    .catch(() => {
-      throw new NotFoundError('Нет карточки с таким id');
-    })
-    .then((data) => {
-      if (req.user._id === data.owner.toString()) {
-        Card.findByIdAndRemove({ _id: data._id })
-          .then(() => {
-            res.status(200).send({ message: 'Карточка успешно удалена' });
-          })
-          .catch(next);
-      } else {
-        throw new AuthorizationError('Вы не можете удалять карточки других пользователей');
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Запрашиваемые данные не найдены');
       }
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет карточки с таким id');
-      } else {
-        next(err);
+      if (card.owner.toString() != req.user._id) {
+        throw new AuthorizationError('Недостаточно прав');
       }
+      // return Card.findByIdAndDelete(req.params._id);
+      Card.deleteOne(card)
+        .then((data) => {
+          res.send({ messages: `Карточка с id: ${data._id} успешно удалена` });
+        })
+        .catch(next);
     });
+  // .then(() => {
+  //   res.status(200).send({ messages: 'Карточка удалена' });
+  // })
+  // .catch((error) => {
+  //   if (error.name === 'CastError') {
+  //     throw new BadRequestError('Карточка с таким id не найдена');
+  //   }
+  //   next(error);
+  // });
 };
 
+// поставить лайк
 const likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .catch(() => {
-      throw new NotFoundError('Нет карточки с таким id');
-    })
     .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Карточка с таким id не найдена');
+      }
       res.status(200).send(card);
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет карточки с таким id');
-      } else {
-        next(err);
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Карточка с таким id не найдена');
       }
+      next(error);
     });
 };
 
+// убрать лайк
 const dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .catch(() => {
-      throw new NotFoundError('Нет карточки с таким id');
-    })
     .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Карточка с таким id не найдена');
+      }
       res.status(200).send(card);
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет карточки с таким id');
-      } else {
-        next(err);
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Карточка с таким id не найдена');
       }
+      next(error);
     });
 };
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,332 +1,150 @@
 /* eslint-disable max-len */
-// require('dotenv').config();
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-
-// const { NODE_ENV, JWT_SECRET } = process.env;
-
-// const User = require('../models/user.js');
-
-// const BadRequestError = require('../errors/BadRequestError.js');
-// const ConflictError = require('../errors/ConflictError.js');
-// const NotFoundError = require('../errors/NotFoundError.js');
-// const UnauthorizedError = require('../errors/UnauthorizedError.js');
-
-// // получить список юзеров
-// const getUsers = (req, res, next) => {
-//   User.find({})
-//     .then((users) => res.status(200).send(users))
-//     .catch(next);
-// };
-
-// // получить информацию о текущем юзере
-// const getUserProfile = (req, res, next) => {
-//   const userId = req.user._id;
-
-//   User.findById(userId)
-//     .then((user) => {
-//       if (!user) {
-//         throw new NotFoundError('Пользователь с таким id не найден');
-//       }
-//       res.status(200).send(user);
-//     })
-//     .catch((error) => {
-//       if (error.name === 'CastError') {
-//         throw new BadRequestError('Пользователь с таким id не найден');
-//       }
-//       next(error);
-//     });
-// };
-
-// // получить информацию о юзере по id
-// const getUserProfileById = (req, res, next) => {
-//   const { userId } = req.params;
-
-//   User.findById(userId)
-//     .then((user) => {
-//       if (!user) {
-//         throw new NotFoundError('Пользователь с таким id не найден');
-//       }
-//       res.status(200).send(user);
-//     })
-//     .catch((error) => {
-//       if (error.name === 'CastError') {
-//         throw new BadRequestError('Пользователь с таким id не найден');
-//       }
-//       next(error);
-//     });
-// };
-
-// // создать нового юзера
-// const createUser = (req, res, next) => {
-//   const {
-//     name, about, avatar, email, password,
-//   } = req.body;
-
-//   User.findOne({ email })
-//     .then((data) => {
-//       if (data) {
-//         throw new ConflictError('Пользователь уже существует');
-//       }
-//       return bcrypt.hash(password, 10)
-//         .then((hash) => User.create({
-//           name, about, avatar, email, password: hash,
-//         }))
-//         .then((user) => res.status(200).send({
-//           name: user.name,
-//           about: user.about,
-//           avatar: user.avatar,
-//           email: user.email,
-//         }))
-//         .catch((error) => {
-//           if (error.name === 'ValidationError') {
-//             throw new BadRequestError('Введены некорректные данные');
-//           }
-//           next(error);
-//         });
-//     });
-// };
-
-// // обновить информацию в профиле юзера
-// const updateUserProfile = (req, res, next) => {
-//   const { name, about } = req.body;
-
-//   return User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-//     .then((user) => {
-//       if (!user) {
-//         throw new NotFoundError('Пользователь с таким id не найден');
-//       }
-//       res.status(200).send(user);
-//     })
-//     .catch((error) => {
-//       if (error.name === 'CastError') {
-//         throw new BadRequestError('Введены некорректные данные');
-//       }
-//       if (error.name === 'ValidationError') {
-//         throw new BadRequestError('Введены некорректные данные');
-//       }
-//       next(error);
-//     });
-// };
-
-// // обновить аватар в профиле юзера
-// const updateUserAvatar = (req, res, next) => {
-//   const { avatar } = req.body;
-
-//   return User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-//     .then((user) => {
-//       if (!user) {
-//         throw new NotFoundError('Пользователь с таким id не найден');
-//       }
-//       res.status(200).send(user);
-//     })
-//     .catch((error) => {
-//       if (error.name === 'CastError') {
-//         throw new BadRequestError('Введены некорректные данные');
-//       }
-//       if (error.name === 'ValidationError') {
-//         throw new BadRequestError('Введены некорректные данные');
-//       }
-//       next(error);
-//     });
-// };
-
-// // аутентификация
-// const loginUser = (req, res, next) => {
-//   const { email, password } = req.body;
-
-//   return User.findUserByCredentials(email, password)
-//     .then((user) => {
-//       if (!user) {
-//         throw new UnauthorizedError('Пользователь не зарегистрирован');
-//       }
-//       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-//       res.send({ token });
-//     })
-//     .catch((error) => {
-//       next(error);
-//     });
-// };
-
-// module.exports = {
-//   getUsers,
-//   getUserProfile,
-//   getUserProfileById,
-//   createUser,
-//   updateUserProfile,
-//   updateUserAvatar,
-//   loginUser,
-// };
-
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-
-const NotFoundError = require('../errors/NotFoundError');
-const BadRequestError = require('../errors/BadRequestError');
-const ConflictError = require('../errors/ConflictError');
-const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-// Запрос списка пользователей
+const User = require('../models/user.js');
+
+const BadRequestError = require('../errors/BadRequestError.js');
+const ConflictError = require('../errors/ConflictError.js');
+const NotFoundError = require('../errors/NotFoundError.js');
+const UnauthorizedError = require('../errors/UnauthorizedError.js');
+
+// получить список юзеров
 const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch(next);
 };
 
-// Запрос информации о текущем пользователе
+// // получить информацию о текущем юзере
 const getUserProfile = (req, res, next) => {
   const userId = req.user._id;
 
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
-      } else {
-        res.status(200).send({ data: user });
+        throw new NotFoundError('Пользователь с таким id не найден');
       }
+      res.status(200).send(user);
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет пользователя с таким id');
-      } else {
-        next(err);
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Пользователь с таким id не найден');
       }
+      next(error);
     });
 };
 
-// Запрос информации о пользователе по id
+// получить информацию о юзере по id
 const getUserProfileById = (req, res, next) => {
   const { userId } = req.params;
 
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
-      } else {
-        res.status(200).send({ data: user });
+        throw new NotFoundError('Пользователь с таким id не найден');
       }
+      res.status(200).send(user);
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет пользователя с таким id');
-      } else {
-        next(err);
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Пользователь с таким id не найден');
       }
+      next(error);
     });
 };
 
-// Запрос на создание пользователя
+// создать нового юзера
 const createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
 
-  User.findOne({
-    email,
-  })
+  User.findOne({ email })
     .then((data) => {
       if (data) {
-        throw new ConflictError('Пользователь с таким email уже существует.');
+        throw new ConflictError('Пользователь уже существует');
       }
-      return bcrypt.hash(password, 10);
-    })
-    .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
-    .then((user) => res.status(200).send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      email: user.email,
-    }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные');
-      } else {
-        next(err);
-      }
+      return bcrypt.hash(password, 10)
+        .then((hash) => User.create({
+          name, about, avatar, email, password: hash,
+        }))
+        .then((user) => res.status(200).send({
+          name: user.name,
+          about: user.about,
+          avatar: user.avatar,
+          email: user.email,
+        }))
+        .catch((error) => {
+          if (error.name === 'ValidationError') {
+            throw new BadRequestError('Введены некорректные данные');
+          }
+          next(error);
+        });
     });
 };
 
-// Контроллер аутентификации
+// обновить информацию в профиле юзера
+const updateUserProfile = (req, res, next) => {
+  const { name, about } = req.body;
+
+  return User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь с таким id не найден');
+      }
+      res.status(200).send(user);
+    })
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Введены некорректные данные');
+      }
+      if (error.name === 'ValidationError') {
+        throw new BadRequestError('Введены некорректные данные');
+      }
+      next(error);
+    });
+};
+
+// обновить аватар в профиле юзера
+const updateUserAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+
+  return User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь с таким id не найден');
+      }
+      res.status(200).send(user);
+    })
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        throw new BadRequestError('Введены некорректные данные');
+      }
+      if (error.name === 'ValidationError') {
+        throw new BadRequestError('Введены некорректные данные');
+      }
+      next(error);
+    });
+};
+
+// // аутентификация
 const loginUser = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
-    .catch(() => {
-      throw new UnauthorizedError('Требуется авторизация');
-    })
     .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Пользователь не зарегистрирован');
+      }
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
-    .catch(next);
-};
-
-// Запрос на обновление информации в профиле
-const updateUserProfile = (req, res, next) => {
-  const { name, about } = req.body;
-
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    {
-      new: true,
-    },
-  )
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
-      } else {
-        res.status(200).send({ data: user });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет пользователя с таким id');
-      } else if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные');
-      } else {
-        next(err);
-      }
-    });
-};
-
-// Запрос на обновление аватара пользователя
-const updateUserAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
-      } else {
-        res.status(200).send({ data: user });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        throw new BadRequestError('Нет пользователя с таким id');
-      } else if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные');
-      } else {
-        next(err);
-      }
+    .catch((error) => {
+      next(error);
     });
 };
 
